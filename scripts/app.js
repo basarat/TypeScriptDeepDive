20921aec0b5f70e6b837850b3ca3254fac58abb7
basarat <basaratali@gmail.com>
2013-04-28 20:14:08 +1000

    Alternate way for depenecy in TS9

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 define(["require", "exports", "angular", "showdown"], function(require, exports) {
     var angular = require("angular");
-    var Showdown = require("showdown");
+    var showdown = require("showdown");
     exports.app = angular.module("mainApp", [], function () {
     });
     exports.app.directive("markdown", function () {
-        var converter = new Showdown.converter();
+        var converter = new showdown.converter();
         return {
             restrict: 'E',
             link: function (scope, element, attrs) {
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,16 +1,23 @@
-/// <amd-dependency path="angular"/>
-/// <amd-dependency path="showdown"/>
 
 /// <reference path="../def/angular/angular.d.ts" />
 /// <reference path="../def/requirejs/require.d.ts" />
 
+/// <amd-dependency path="angular"/>
+/// <amd-dependency path="showdown"/>
+
+// Alternate way for dependency for TS 9
+//declare module "angular"{}
+//declare module "showdown"{}
+//import angular = module("angular");
+//import showdown= module("showdown");
+
 var angular = require("angular");
-var Showdown = require("showdown");
+var showdown = require("showdown");
 
 export var app = angular.module("mainApp",[],function(){});
 
 app.directive("markdown",function(){
-    var converter = new Showdown.converter();
+    var converter = new showdown.converter();
     return{
         restrict: 'E',
         link:function(scope,element,attrs) {
